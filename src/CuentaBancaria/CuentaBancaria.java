fbb2265fda2639e5013b3c4aaa33ef7c1bd7579d
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 14:49:49 -0500

    elaboracion de constructor con dos perametros

--- src/CuentaBancaria/CuentaBancaria.java
+++ src/CuentaBancaria/CuentaBancaria.java
@@ -5,4 +5,11 @@ public class CuentaBancaria {
     public String numeroCuenta;
     public double saldo;
     public String tipoCuenta;
+
+    // 2. Constructor parametrizado con dos parámetros (numeroCuenta y tipoCuenta)
+    public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = 0.0; // Saldo inicial por defecto
+        this.tipoCuenta = tipoCuenta;
+    }
 }
