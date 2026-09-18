90f6a8478c2548c21fa5734bb4822c12deee3ed9
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 15:00:22 -0500

    elaboracion de constructor generico

--- src/Estudiante/Estudiante.java
+++ src/Estudiante/Estudiante.java
@@ -4,4 +4,9 @@ public class Estudiante {
     public String nombre;
     public int edad;
     public String curso;
+
+    // 1. Constructor por defecto que asigne valores genéricos (llama al constructor parametrizado)
+    public Estudiante() {
+        this("Sin nombre", 0, "Sin curso");
+    }
 }
