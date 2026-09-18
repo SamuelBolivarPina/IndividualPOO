cb1fb0074f906c93242ce68be31dece94d554e26
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 14:40:57 -0500

    elaboracion de metodos para mostar detalles del libor como autor titulo numero de paginas

--- Libro/Libro.java
+++ Libro/Libro.java
@@ -17,4 +17,28 @@ public class Libro {
         this.autor = autor;
         this.numeroPaginas = numeroPaginas;
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
 }
