3420774d8f73bc3ad4d8e2defc6e4aeeaaa2e327
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 14:34:35 -0500

    elavoracion de constructor por defecto aignando valores predeterminados

--- Libro/Libro.java
+++ Libro/Libro.java
@@ -3,4 +3,11 @@ public class Libro {
     public String titulo;
     public String autor;
     public int numeroPaginas;
+
+    // 1. Constructor por defecto (asigna valores predeterminados)
+    public Libro() {
+        this.titulo = "Sin título";
+        this.autor = "Autor desconocido";
+        this.numeroPaginas = 0;
+    }
 }
